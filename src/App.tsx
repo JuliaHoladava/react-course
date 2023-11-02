8afd1f720598c6c52e07248a3cfe396bac59cbef
Julia Holadava <juliaholadava@gmail.com>
2023-11-02 13:37:29 +0100

    fix: migrate component from a class to a function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import { useCallback, useState } from 'react';
 import SearchBar from './SearchBar';
 import Results from './Results';
-import { AppState } from './interfaces';
 import ErrorBoundary from './ErrorBoundary';
 
 async function fetchResults(searchTerm: string) {
@@ -12,44 +11,33 @@ async function fetchResults(searchTerm: string) {
   return data.results;
 }
 
-class App extends React.Component<object, AppState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-      results: [],
-    };
-  }
+const App = () => {
+  const [results, setResults] = useState([]);
 
-  setSearchTerm = async (term: string) => {
-    this.setState({ searchTerm: term });
-
+  const handleSetSearchTerm = useCallback(async (term: string) => {
     try {
-      const results = await fetchResults(term);
-      this.setState({ results });
+      const fetchedResults = await fetchResults(term);
+      setResults(fetchedResults);
     } catch (error) {
       console.error('Executing request error', error);
     }
-  };
+  }, []);
 
-  render() {
-    const { results } = this.state;
-    return (
-      <ErrorBoundary>
-        <button
-          className="button-error"
-          type="button"
-          onClick={() => {
-            throw new Error('Test error');
-          }}
-        >
-          Generate Error
-        </button>
-        <SearchBar setSearchTerm={this.setSearchTerm} />
-        <Results results={results} />
-      </ErrorBoundary>
-    );
-  }
-}
+  return (
+    <ErrorBoundary>
+      <button
+        className="button-error"
+        type="button"
+        onClick={() => {
+          throw new Error('Test error');
+        }}
+      >
+        Generate Error
+      </button>
+      <SearchBar setSearchTerm={handleSetSearchTerm} />
+      <Results results={results} />
+    </ErrorBoundary>
+  );
+};
 
 export default App;
