e863953cfa042da9e2917de3be898bb271d5a1fa
Julia Holadava <juliaholadava@gmail.com>
2023-11-19 19:29:18 +0100

    feat: update Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { starWarsCharactersApi } from '../api/starWarsCharactersApi';
 import { searchReducer } from './reducers/searchReducer';
+import { viewModelReducer } from './reducers/viewModelReducer';
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    viewModel: viewModelReducer,
+    [starWarsCharactersApi.reducerPath]: starWarsCharactersApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(starWarsCharactersApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
