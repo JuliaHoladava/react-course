708f45348fc6f35c08fd26acdf644c790d94124d
Julia Holadava <juliaholadava@gmail.com>
2023-11-19 19:05:09 +0100

    feat: update component to integrate with Redux

--- src/pages/home/App.tsx
+++ src/pages/home/App.tsx
@@ -2,29 +2,32 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Results from '../../components/Result-list/Results';
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
-import { SearchProvider } from '../../components/SearchContext/SearchContext';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <SearchProvider>
-        <ErrorBoundary>
-          <button
-            className="button-error"
-            type="button"
-            onClick={() => {
-              throw new Error('Test error');
-            }}
-          >
-            Generate Error
-          </button>
-          <Routes>
-            <Route path="/" element={<SearchBar />}>
-              <Route index element={<Results />} />
-            </Route>
-          </Routes>
-        </ErrorBoundary>
-      </SearchProvider>
+      <ErrorBoundary>
+        <button
+          className="button-error"
+          type="button"
+          onClick={() => {
+            throw new Error('Test error');
+          }}
+        >
+          Generate Error
+        </button>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <SearchBar />
+                <Results />
+              </div>
+            }
+          />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
