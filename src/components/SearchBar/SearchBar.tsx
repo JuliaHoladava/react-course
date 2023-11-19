3c9b88467ca5eadb0b5231fc4aab30d35a59dc6f
Julia Holadava <juliaholadava@gmail.com>
2023-11-19 21:55:11 +0100

    feat: update search bar to integrate with redux

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,37 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSearchTerm } from '../../redux/reducers/searchReducer';
+import {
+  setCount,
+  setPage,
+  setLoading,
+} from '../../redux/reducers/viewModelReducer';
+import { useFetchCharactersQuery } from '../../api/starWarsCharactersApi';
 import { RootState } from '../../redux/store';
 import './SearchBar.css';
 import '../../pages/home/App.css';
 
 const SearchBar = () => {
-  const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
   const dispatch = useDispatch();
+  const { searchTerm } = useSelector((state: RootState) => state.search);
+  const { page } = useSelector((state: RootState) => state.viewModel);
+  const { data, error, isLoading } = useFetchCharactersQuery({
+    searchTerm,
+    page,
+  });
 
   useEffect(() => {
     const storedSearchTerm = localStorage.getItem('lastSearch') || '';
     dispatch(setSearchTerm(storedSearchTerm));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (data) {
+      dispatch(setCount(data.count));
+      dispatch(setPage(page));
+    }
+    dispatch(setLoading(isLoading));
+  }, [data, isLoading, dispatch, page]);
 
   const handleSearch = () => {
     const trimmedTerm = searchTerm.trim();
@@ -31,6 +50,8 @@ const SearchBar = () => {
       <button className="button-search" type="submit" onClick={handleSearch}>
         Search
       </button>
+      {error ? <p>Error loading data</p> : null}
+      {isLoading ? <p>Loading...</p> : null}
     </div>
   );
 };
