6ed19a0ad0880dc52ddc07e38818c015c0f2d6d4
Julia Holadava <juliaholadava@gmail.com>
2023-11-23 16:14:07 +0100

    refactor: optimize Details with destructuring

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -2,28 +2,29 @@ import React from 'react';
 import { DetailsProps } from '../../type/interfaces';
 
 const Details: React.FC<DetailsProps> = ({ character }) => {
+  const {
+    name,
+    height,
+    mass,
+    hair_color: hairColor,
+    skin_color: skinColor,
+    eye_color: eyeColor,
+    birth_year: birthYear,
+    gender,
+    homeworld,
+  } = character;
   return (
     <div>
       <div>
-        <h2>{character.name}</h2>
-        <p>Height: {character.height ? character.height : 'No data'}</p>
-        <p>Mass: {character.mass ? character.mass : 'No data'}</p>
-        <p>
-          Hair color: {character.hair_color ? character.hair_color : 'No data'}
-        </p>
-        <p>
-          Skin color: {character.skin_color ? character.skin_color : 'No data'}
-        </p>
-        <p>
-          Eye color: {character.eye_color ? character.eye_color : 'No data'}
-        </p>
-        <p>
-          Birth year: {character.birth_year ? character.birth_year : 'No data'}
-        </p>
-        <p>Gender: {character.gender ? character.gender : 'No data'}</p>
-        <p>
-          Homeworld: {character.homeworld ? character.homeworld : 'No data'}
-        </p>
+        <h2>{name}</h2>
+        <p>Height: {height || 'No data'}</p>
+        <p>Mass: {mass || 'No data'}</p>
+        <p>Hair color: {hairColor || 'No data'}</p>
+        <p>Skin color: {skinColor || 'No data'}</p>
+        <p>Eye color: {eyeColor || 'No data'}</p>
+        <p>Birth year: {birthYear || 'No data'}</p>
+        <p>Gender: {gender || 'No data'}</p>
+        <p>Homeworld: {homeworld || 'No data'}</p>
       </div>
     </div>
   );
